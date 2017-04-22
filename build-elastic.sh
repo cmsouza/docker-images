42e862fd2f9ebc0a606cd95cde5a26705f64ff84
Ciro Souza <ciro.mies@gmail.com>
2017-04-22 01:10:45 -0300

    curl trigger

--- build-elastic.sh
+++ build-elastic.sh
@@ -3,3 +3,6 @@ cd elastic
 VERSION=0.0.1
 
 docker build -t cmsouza/elasticsearch:${VERSION} .
+
+#trigger remote build
+#curl -H "Content-Type: application/json" --data '{"docker_tag": "0.0.1+elastic"}' -X POST https://registry.hub.docker.com/u/cmsouza/docker-images/trigger/{token}/
