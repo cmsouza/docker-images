dbad4f951d8b082e3410128d27c53be26be758bd
Ciro Souza <ciro.mies@gmail.com>
2017-08-11 00:18:46 -0300

    Alterando tag da imagem

--- build-elastic.sh
+++ build-elastic.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd elastic
-VERSION=0.0.1
 
-docker build -t cmsouza/elasticsearch:${VERSION} .
+docker build -t cmsouza/docker-images:elastic .
 
 #trigger remote build
 #curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "0.0.1+elastic"}' -X POST https://registry.hub.docker.com/u/cmsouza/docker-images/trigger/{token}/
